5ac23a72f6738c52c1313195ad0ee136b8d20fbe
Damanvir Singh <damansingh864@gmail.com>
2020-04-09 23:51:48 +0530

    Add Higher Order Component for our spinner and setup collections in firebase and implement styled components"

--- src/App.js
+++ src/App.js
@@ -14,13 +14,16 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from './firebase/firebase.util'
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selectors'
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+
     const { setCurrentUser }= this.props
+    // const { setCurrentUser, collectionArray }= this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -36,8 +39,9 @@ class App extends React.Component {
       } else {
 
         setCurrentUser(userAuth)
-
+        
       }
+      // addCollectionAndDocuments('collections', collectionArray.map(({title, items}) => ({ title, items })))
     })
   }
  
@@ -62,6 +66,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+  // collectionArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ match }) => (
-  <div className='show-page'>
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-)
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
-export default ShopPage
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.util'
+
+import { updateCollections } from '../../redux/shop/shop.actions'
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
+class ShopPage extends React.Component {  
+  state = {
+    loading: true
+  }
+  
+  unSubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props
+    const collectionRef = firestore.collection('collections')
+
+    this.unSubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot =>{
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot)
+      console.log(collectionMap)
+      updateCollections(collectionMap)
+      this.setState({ loading: false })
+    })
+  }
+
+  render () {
+    const { match } = this.props
+    const { loading } = this.state
+    return (
+      <div className='show-page'>
+        <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+        />
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage)
